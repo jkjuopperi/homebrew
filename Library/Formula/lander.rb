52330eb35c60854a82c4e200d5675218f2d97bb4
Juho Juopperi <jkj@kapsi.fi>
2011-10-12 21:11:35 +0300

    Added (Lunar) Lander 0.6.2 http://www.doof.me.uk/lunar-lander/

---
+++ Library/Formula/lander.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+class Lander < Formula
+  url 'http://www.nickg.me.uk/files/lander-0.6.2.tar.gz'
+  homepage 'http://www.doof.me.uk/lunar-lander/'
+  md5 'd2735ecbfbd58f017ea2c8b8b9e29a19'
+
+  depends_on 'boost'
+  depends_on 'gettext'
+  depends_on 'sdl'
+  depends_on 'sdl_image'
+  depends_on 'sdl_mixer'
+
+  def patches
+    DATA
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "-with-boost=#{HOMEBREW_PREFIX}"
+    system "make install"
+  end
+end
+__END__
+diff --git a/src/Main.cpp b/src/Main.cpp
+index 1e5b257..7dc9913 100644
+--- a/src/Main.cpp
++++ b/src/Main.cpp
+@@ -238,14 +238,14 @@ string GetConfigDir()
+    p /= "lander";
+    create_directories(p);
+ 
+-   return p.file_string() + "/";
++   return p.string() + "/";
+ #else
+ #ifdef WIN32
+    path appdata(getenv("APPDATA"));
+    appdata /= "doof.me.uk";
+    appdata /= "Lander";
+    create_directories(appdata);
+-   return appdata.file_string() + "\\";
++   return appdata.string() + "\\";
+ #else
+ #error "Need to port GetConfigDir to this platform"
+ #endif
+
